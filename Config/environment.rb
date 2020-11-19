1fdf7e53a780b40793bca88733c16db0069515c6
amcmullen9291 <amcmullen9291@gmail.com>
2020-11-19 17:47:45 -0500

    config setup properly

--- Config/environment.rb
+++ Config/environment.rb
@@ -1,25 +1,14 @@
 require 'bundler'
 require 'require_all'
 
-ENV['SINATRA_ENV'] ||="development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-configure :development do
-  set :database, 'sqlite3:db/BeerCheeseSoup.db'
-end
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-configure do
-  set :environment, :production
-  enable :sessions
-  set :session_secret, "overdraftprotection"
-end
-
 require_all './mvc'
+
+ENV[‘SINATRA_ENV’] ||= “development”
+require ‘bundler/setup’
+Bundler.require(:default, ENV[‘SINATRA_ENV’])
+ActiveRecord::Base.establish_connection(
+:adapter => “sqlite3”,
+:database => “db/#{ENV[‘SINATRA_ENV’]}.sqlite”
+
+require_all {'mvc'} )
 
