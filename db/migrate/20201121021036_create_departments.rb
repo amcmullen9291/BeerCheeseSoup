ca183a9cf0c28fbf4eea5824914d77ca9d63767c
amcmullen9291 <amcmullen9291@gmail.com>
2020-11-21 15:38:49 -0500

    commit before migrations

--- db/migrate/20201121021036_create_departments.rb
+++ db/migrate/20201121021036_create_departments.rb
@@ -1,8 +1,11 @@
 class CreateDepartments < ActiveRecord::Migration[6.0]
+
   def change
     unless ActiveRecord::Base.connection.table_exists?('Departments')
-    create_table :Departments do |t|
-      t.string :name
+      create_table :Departments do |t|
+        t.string :name
+      end
     end
   end
+
 end
